f75d212e8acf3f0f003c1f183c12389a1c6615bf
Sam Gaudet <sgaudet1@babson.edu>
2019-07-05 14:08:37 -0400

    updated testing and execution
    Removed testing code from scraper.py and added main.py executable file for running the program

---
+++ main.py
@@ -0,0 +1,5 @@
+import scraper
+
+scraper = scraper.Scraper("/Users/samgaudet/Documents/GitHub/taser_pdf/test_pdfs/")
+
+scraper.scrape()
--- scraper.py
+++ scraper.py
@@ -195,6 +195,5 @@ class Scraper:
 
 
 
-scraper = Scraper("/Users/samgaudet/Documents/GitHub/taser_pdf/test_pdfs/")
 
-scraper.scrape()
+
