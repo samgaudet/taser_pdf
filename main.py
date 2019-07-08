879538a0caae87cf5f77b5bd05efcc56b2d4f22b
Sam Gaudet <sgaudet1@babson.edu>
2019-07-07 22:07:10 -0400

    improve variable names

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import scraper
 
-scraper = scraper.Scraper(filepath="/Users/samgaudet/Documents/GitHub/taser_pdf/test_pdfs/")
+myScraper = scraper.Scraper(filepath="/Users/samgaudet/Documents/GitHub/taser_pdf/test_pdfs/")
 
-scraper.scrape()
+myScraper.scrape()
